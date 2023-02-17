c8a07db9e935e2eab0f1c3b3d97d1369c8e15b79
Kiran158 <kbb1582001@gmail.com>
2023-02-17 15:07:57 +0530

    I changed name of the app

--- card/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class products(models.Model):
-    product_id = models.AutoField
-    product_name = models.CharField(max_length=50)
-    description = models.CharField(max_length=300)
-    publish_date = models.DateField
